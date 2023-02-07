1472573dc8fa5cca1aa50586f3dfaa38d2315d7c
Ruben <ruben@kislaki.net>
2023-02-07 18:59:19 +0200

    added spinner

--- packages/ui/lib/index.ts
+++ packages/ui/lib/index.ts
@@ -6,3 +6,4 @@ export { Input } from "./Input";
 export { InputGroup } from "./InputGroup";
 export { Label } from "./Label";
 export { Link } from "./Link";
+export { Spinner } from "./Spinner";
---
+++ packages/ui/stories/Spinner.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Spinner } from "../lib";
+
+export default {
+  title: "Example/Spinner",
+  component: Spinner,
+} as ComponentMeta<typeof Spinner>;
+
+const Template: ComponentStory<typeof Spinner> = (args) => (
+  <Spinner {...args} />
+);
+
+export const Default = Template.bind({});
