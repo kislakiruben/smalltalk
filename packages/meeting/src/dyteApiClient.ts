7cf42fb7d59f1e5c284c61d98e00ad2880a26eb8
Ruben <ruben@kislaki.net>
2023-02-08 14:21:21 +0200

    created axios client to call dyte rest api

---
+++ packages/meeting/src/dyteApiClient.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://api.cluster.dyte.in/v2",
+  auth: {
+    username: process.env.REACT_APP_DYTE_ORG_ID,
+    password: process.env.REACT_APP_DYTE_API_KEY,
+  },
+});
