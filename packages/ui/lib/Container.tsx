3ee51eabfea0bf2de24c49ffe5fb81ec689e6b62
Ruben <ruben@kislaki.net>
2023-02-08 18:15:06 +0200

    remove unused component

--- packages/ui/lib/Container.tsx
+++
@@ -1,7 +0,0 @@
-interface ContainerProps {
-  children: React.ReactNode;
-}
-
-export const Container = ({ children }: ContainerProps) => {
-  return <div className="px-6 w-full max-w-screen-xl mx-auto">{children}</div>;
-};
--- packages/ui/lib/index.ts
+++ packages/ui/lib/index.ts
@@ -2,7 +2,6 @@ import "./tailwind.css";
 
 export { AuthForm } from "./AuthForm";
 export { Button } from "./Button";
-export { Container } from "./Container";
 export { Header } from "./Header";
 export { Input } from "./Input";
 export { InputGroup } from "./InputGroup";
