fd107643bd6d7354f5bc03c61b51eb02dc5036a7
Ruben <ruben@kislaki.net>
2023-02-08 19:40:17 +0200

    added basic form handling when saving profile info

--- packages/profile/src/components/AccountPassword.tsx
+++ packages/profile/src/components/AccountPassword.tsx
@@ -1,18 +1,60 @@
 import { Button, Label, Input } from "@dyteio/ui";
+import { useState } from "react";
+
+import supabase from "../supabaseClient";
 
 const AccountPassword = () => {
+  const [isSaving, setIsSaving] = useState(false);
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const onChangeOldPassword = (event: React.FormEvent<HTMLInputElement>) => {
+    setOldPassword((event.target as HTMLInputElement).value);
+  };
+  const onChangeNewPassword = (event: React.FormEvent<HTMLInputElement>) => {
+    setNewPassword((event.target as HTMLInputElement).value);
+  };
+  const asyncSubmit = async () => {
+    setIsSaving(true);
+    try {
+      setIsSaving(false);
+    } catch (e) {
+      setIsSaving(false);
+    }
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    asyncSubmit();
+    event.preventDefault();
+  };
+
   return (
     <form className="section">
       <h3 className="section__title">Password</h3>
       <div className="section__group">
         <Label htmlFor="currentPassword">Old password:</Label>
-        <Input id="currentPassword" type="text" />
+        <Input
+          autoComplete="current-password"
+          id="currentPassword"
+          onChange={onChangeOldPassword}
+          type="password"
+          value={oldPassword}
+        />
       </div>
       <div className="section__group">
         <Label htmlFor="newPassword">Old password:</Label>
-        <Input id="newPassword" type="password" />
+        <Input
+          autoComplete="new-password"
+          id="newPassword"
+          onChange={onChangeNewPassword}
+          type="password"
+          value={newPassword}
+        />
       </div>
-      <Button className="section__button" primary type="submit">
+      <Button
+        className="section__button"
+        disabled={isSaving}
+        primary
+        type="submit"
+      >
         Change password
       </Button>
     </form>
--- packages/profile/src/components/AccountProfile.tsx
+++ packages/profile/src/components/AccountProfile.tsx
@@ -1,26 +1,80 @@
 import { Button, Label, Input } from "@dyteio/ui";
+import { useEffect, useRef, useState } from "react";
 
 const AccountProfile = () => {
+  const emailInputRef = useRef<HTMLInputElement>(null);
+  const [isSaving, setIsSaving] = useState(false);
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [bio, setBio] = useState("");
+  const [email, setEmail] = useState("");
+  const onChangeName = (event: React.FormEvent<HTMLInputElement>) => {
+    setName((event.target as HTMLInputElement).value);
+  };
+  const onChangeLocation = (event: React.FormEvent<HTMLInputElement>) => {
+    setLocation((event.target as HTMLInputElement).value);
+  };
+  const onChangeBio = (event: React.FormEvent<HTMLInputElement>) => {
+    setBio((event.target as HTMLInputElement).value);
+  };
+  const onChangeEmail = (event: React.FormEvent<HTMLInputElement>) => {
+    setEmail((event.target as HTMLInputElement).value);
+  };
+  const asyncSubmit = async () => {
+    setIsSaving(true);
+    try {
+      setIsSaving(false);
+    } catch (e) {
+      setIsSaving(false);
+    }
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    asyncSubmit();
+    event.preventDefault();
+  };
+
+  useEffect(() => {
+    if (emailInputRef.current) {
+      emailInputRef.current.focus();
+    }
+  }, []);
+
   return (
-    <form className="section">
+    <form className="section" onSubmit={onSubmit}>
       <h3 className="section__title">Profile</h3>
       <div className="section__group">
+        <Label htmlFor="email">Email:</Label>
+        <Input
+          id="email"
+          onChange={onChangeEmail}
+          ref={emailInputRef}
+          type="text"
+          value={email}
+        />
+      </div>
+      <div className="section__group">
         <Label htmlFor="name">Name:</Label>
-        <Input id="name" type="text" />
+        <Input id="name" onChange={onChangeName} type="text" value={name} />
       </div>
       <div className="section__group">
         <Label htmlFor="location">Location:</Label>
-        <Input id="bio" type="text" />
+        <Input
+          id="location"
+          onChange={onChangeLocation}
+          type="text"
+          value={location}
+        />
       </div>
       <div className="section__group">
         <Label htmlFor="bio">Bio:</Label>
-        <Input id="bio" type="text" />
+        <Input id="bio" onChange={onChangeBio} type="text" value={bio} />
       </div>
-      <div className="section__group">
-        <Label htmlFor="email">Email:</Label>
-        <Input id="email" type="text" />
-      </div>
-      <Button className="section__button" primary type="submit">
+      <Button
+        className="section__button"
+        disabled={isSaving}
+        primary
+        type="submit"
+      >
         Save profile
       </Button>
     </form>
