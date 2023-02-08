114b6d3c0cc0ab3f890880c15fdf4b07fe969e6f
Ruben <ruben@kislaki.net>
2023-02-08 21:03:33 +0200

    update storage strategy for supabase client

--- packages/meeting/src/supabaseClient.ts
+++ packages/meeting/src/supabaseClient.ts
@@ -1,10 +1,23 @@
+import cookies from "js-cookie";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || "";
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_PUB_KEY || "";
+const cookieOptions = { expires: 100 * 365 * 24 * 60 * 60 };
 
 export default createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    persistSession: false,
+    // persistSession: false,
+    storage: {
+      getItem: (key) => {
+        return cookies.get(key);
+      },
+      setItem: (key, value) => {
+        cookies.set(key, value, cookieOptions);
+      },
+      removeItem: (key) => {
+        cookies.remove(key);
+      },
+    },
   },
 });
