12da4143a1d9fcb9ece9d58579cc0cc948fdff2d
Ruben <ruben@kislaki.net>
2023-02-08 22:12:11 +0200

    update handling of onboarding

--- packages/meeting/src/components/Meetings.tsx
+++ packages/meeting/src/components/Meetings.tsx
@@ -1,23 +1,37 @@
 import { Button } from "@dyteio/ui";
-import { useRecoilValue } from "recoil";
+import { useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
 
 import { currentAuthTokenState, meetingsState } from "../atoms/dyte";
 import DyteWrapper from "./DyteWrapper";
 import Meeting from "./Meeting";
+import dyteApiClient from "../dyteApiClient";
 
 const Meetings = () => {
   const authToken = useRecoilValue(currentAuthTokenState);
-  const meetings = useRecoilValue(meetingsState);
+  const [meetings, setMeetings] = useRecoilState(meetingsState);
+  const [isCreating, setIsCreating] = useState(false);
+  const onCreateMeeting = async () => {
+    setIsCreating(true);
+    try {
+      const response = await dyteApiClient.post("/meetings", {
+        title: "This meeting should have been an email",
+        preferred_region: "ap-south-1",
+      });
+
+      setMeetings((currentMeetings): [] => {
+        return [...currentMeetings, response.data.data] as [];
+      });
+      setIsCreating(false);
+    } catch (e) {
+      setIsCreating(false);
+    }
+  };
 
   return authToken ? (
     <DyteWrapper authToken={authToken} />
   ) : (
     <div className="meetings">
-      <div className="meetings__header">
-        <Button primary type="button">
-          Create a new meeting
-        </Button>
-      </div>
       {meetings.length > 0 ? (
         <ul className="meetings__list">
           {meetings.map((meeting) => (
@@ -29,6 +43,14 @@ const Meetings = () => {
       ) : (
         <div className="meetings__empty">
           <p>There are not meetings</p>
+          <Button
+            disabled={isCreating}
+            onClick={onCreateMeeting}
+            primary
+            type="button"
+          >
+            Create a new meeting
+          </Button>
         </div>
       )}
     </div>
