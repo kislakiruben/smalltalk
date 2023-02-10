4e3d91d48dbcbd3bd42becc0ca369d7010b74f30
Ruben <ruben@kislaki.net>
2023-02-10 14:41:34 +0200

    fix permissions

--- packages/lobbies/src/index.tsx
+++ packages/lobbies/src/index.tsx
@@ -16,6 +16,9 @@ root.render(
       domain={process.env.REACT_APP_AUTH0_DOMAIN as string}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID as string}
       authorizationParams={{
+        audience: process.env.REACT_APP_AUTH0_AUDIENCE as string,
+        scope:
+          "openid profile email read:current_user update:current_user_metadata",
         redirect_uri: window.location.origin,
       }}
     >
